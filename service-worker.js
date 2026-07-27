3b858f4a679df9b3586dc43f55edc566cf71ac08
Hasanmehdi6069 <shanh1346@gmail.com>
2026-07-28 00:09:58 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+// service-worker.js
+const CACHE_NAME = 'axelr-v4';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css?v=20260727',
+    '/script.js?v=20260727',
+    '/favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
